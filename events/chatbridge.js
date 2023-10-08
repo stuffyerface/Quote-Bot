2a7dd88efa29af77a91407fecdffe339daf3d4e1
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2023-10-08 04:07:39 -0400

    attempt to fix message too short

--- events/chatbridge.js
+++ events/chatbridge.js
@@ -5,6 +5,10 @@ module.exports = {
     if (!config.webhookid || !config.webhooktoken) return;
     const webhookClient = new Discord.WebhookClient(config.webhookid, config.webhooktoken);
     const msg = messageContent.toString()
+    if (msg.length < 1) {
+      console.log("Message too short to be sent to webhook.")
+      return;
+    }
     webhookClient.send({
       username: `${author}`,
       avatarURL: `https://minotar.net/helm/${author}/512`,
