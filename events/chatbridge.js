c0e4cec99645d894c156af585b3628fe55c636de
Stuffy <stuffinessmc@gmail.com>
2023-10-08 04:13:06 -0400

    Try catch webhook

--- events/chatbridge.js
+++ events/chatbridge.js
@@ -5,15 +5,16 @@ module.exports = {
     if (!config.webhookid || !config.webhooktoken) return;
     const webhookClient = new Discord.WebhookClient(config.webhookid, config.webhooktoken);
     const msg = messageContent.toString()
-    if (msg.length < 1) {
-      console.log("Message too short to be sent to webhook.")
-      return;
+
+    try {
+      webhookClient.send({
+        username: `${author}`,
+        avatarURL: `https://minotar.net/helm/${author}/512`,
+        content: `${msg}`,
+        allowedMentions: { "parse": [] }
+      })
+    } catch (err) {
+      console.log(err)
     }
-    webhookClient.send({
-      username: `${author}`,
-      avatarURL: `https://minotar.net/helm/${author}/512`,
-      content: `${msg}`,
-      allowedMentions: { "parse": [] }
-    })
   }
 }
