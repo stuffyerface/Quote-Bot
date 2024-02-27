edf41327c209ce30c76c8f00a27bb254b2fdaff8
Stuffy <stuffinessmc@gmail.com>
2024-02-27 15:19:43 -0500

    Cap names to 16 long, trim "Ez"

--- client/chatbridge.js
+++ client/chatbridge.js
@@ -5,6 +5,7 @@ module.exports = {
     if (text.startsWith('sudo')) return;
     if (message.channel.id === config.chatchannel) {
       displayName = message.member.displayName
+      displayName = displayName.slice(0,15)
       // Check if message is a reply
       if (message.reference) {
         const repliedMessage = message.channel.messages.cache.get(message.reference.messageId);
@@ -15,40 +16,59 @@ module.exports = {
         } else {
           repliedUser = repliedMessage.member.displayName
         }
-        command = `/gc ${displayName} replying to ${repliedUser}: ${text}`
+        prefix = `${displayName} replying to ${repliedUser}: `
       } else {
-        command = `/gc ${displayName}: ${text}`
+        prefix = `${displayName}: `
       }
 
-      command = command.replaceAll("\n", " ") // Remove newlines
-      command = command.replace(/<:([^:]+):\d+>/g, ":$1:") // Remove custom emojis 
+      text = text.replaceAll("\n", " ") // Remove newlines
+      text = text.replace(/<:([^:]+):\d+>/g, ":$1:") // Remove custom emojis 
 
-      command = command.replace(/<@(\d+)>/g, (match, id) => `@${client.users.cache.get(id).globalName}`) // Replace user mentions with usernames
-      command = command.replace(/<#(\d+)>/g, (match, id) => `#${client.channels.cache.get(id).name}`) // Replace channel mentions with channel names
+      text = text.replace(/<@(\d+)>/g, (match, id) => `@${client.users.cache.get(id).globalName}`) // Replace user mentions with usernames
+      text = text.replace(/<#(\d+)>/g, (match, id) => `#${client.channels.cache.get(id).name}`) // Replace channel mentions with channel names
       // command = command.replace(/<@&(\d+)>/g, (match, id) => `@${client.roles.cache.get(id).name}`) // Replace role mentions with role names DOES NOT WORK
+      text = text.replace(/\be+z+\b/gi, function (match) {
+        return "*".repeat(match.length); // Censor "ez"
+      })
+
+      text = text.replace(/\bi+p+\b/gi, function (match) {
+        return "*".repeat(match.length); // Censor "ip"
+      })
+
+      command = `/gc ` + prefix + text
 
       if (command.length > 256) {
         command = command.slice(0, 253) + "..."
       }
 
-      // remove ez's
-      command = command.replace(/\be+z+\b/gi, function(match){ 
-        return "*".repeat(match.length);
-      })
+      if (text.length != 0) {
+        try {
+          await bot.chat(command)
+        } catch (error) {
+          console.log(error)
+        }
+      }
 
-      bot.chat(command)
-      // TODO: Upload images to imgur and send link instead.
       // const attachments = (message.attachments).array();
-      // if (attachments.length > 0) {
-      //   if (nick1) {
-      //     setTimeout(() => {
-      //       bot.chat(`/gc ${nick1}: ${attachments[0].url}`)
-      //     }, 550);
-      //   } else {
-      //     setTimeout(() => {
-      //       bot.chat(`/gc ${message.author.username}: ${attachments[0].url}`)
-      //     }, 550)
-      //   }
+
+      // for (attachment of attachments) {
+      //   const url = attachment.url;
+      //   const filename = attachment.name;
+      //   const imgur = require('imgur');
+      //   imgur.setClientId(config.imgur_client_id);
+      //   imgur.setAPIUrl('https://api.imgur.com/3/');
+      //   imgur.uploadUrl(url)
+      //     .then(function (json) {
+      //       const link = json.data.link;
+      //       if (link) {
+      //         setTimeout(() => {
+      //           bot.chat(`/gc ${prefix}: ${link}`)
+      //         }, 550);
+      //       }
+      //     })
+      //     .catch(function (err) {
+      //       console.error(err.message);
+      //     });
       // }
     }
   }
